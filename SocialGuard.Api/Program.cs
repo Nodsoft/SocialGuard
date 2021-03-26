15297ccd41b560cd32356bb179d6d50efd14fb4b
Sakura Isayeki <sakura.isayeki@nodsoft.net>
2021-03-26 15:40:29 +0100

    Switch logging to Serilog

--- SocialGuard.Api/Program.cs
+++ SocialGuard.Api/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MongoDB.Bson.Serialization.Conventions;
+using Serilog;
 using System.Threading.Tasks;
 
 namespace SocialGuard.Api
@@ -12,6 +15,24 @@ namespace SocialGuard.Api
 			ConventionRegistry.Register("Ignore null values", new ConventionPack { new IgnoreIfNullConvention(true) }, t => true);
 
 			using IHost host = CreateHostBuilder(args).Build();
+			using IServiceScope scope = host.Services.CreateScope();
+
+			IConfiguration configuration = scope.ServiceProvider.GetRequiredService<IConfiguration>();
+
+			Log.Logger = new LoggerConfiguration()
+			#if DEBUG
+				.MinimumLevel.Verbose()
+			#else
+				.MinimumLevel.Information()
+				.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+				.MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+			#endif
+				.Enrich.FromLogContext()
+				.Enrich.WithProperty("_Source", typeof(Program).Assembly.GetName())
+				.Enrich.WithProperty("_Environment", configuration["environment"])
+				.WriteTo.Console()
+//				.WriteTo.Logger(fileLogger)
+				.CreateLogger();
 
 			await host.RunAsync();
 		}
@@ -21,6 +42,7 @@ namespace SocialGuard.Api
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
 					webBuilder.UseStartup<Startup>();
+					webBuilder.UseSerilog();
 				});
 	}
 }
