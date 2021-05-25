72bd323e30bde114c71d41ea39ce5aa496f95e97
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2021-05-25 09:22:12 +0200

    (WIP) Draft Multi-Entry data system

---
+++ SocialGuard.Api/Data/Models/TrustlistEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SocialGuard.Api.Data.Models
+{
+	public record TrustlistEntry
+	{
+		[BsonRequired, BsonRepresentation(BsonType.ObjectId)]
+		public ObjectId Id { get; init; }
+
+		[BsonRequired]
+		public DateTime EntryAt { get; init; }
+
+		[BsonRequired]
+		public DateTime LastEscalated { get; set; }
+
+		[Required, BsonRequired, Range(0, 3)]
+		public byte EscalationLevel { get; set; }
+
+		[Required, BsonRequired, MinLength(5), MaxLength(2000)]
+		public string EscalationNote { get; set; }
+
+		[BsonRequired]
+		public Emitter Emitter { get; set; }
+	}
+}
--- SocialGuard.Api/Services/TrustlistUserService.cs
+++ SocialGuard.Api/Services/TrustlistUserService.cs
@@ -1,12 +1,10 @@
 ﻿using MongoDB.Driver;
+using SharpCompress.Common;
 using SocialGuard.Api.Data.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
-
-
 namespace SocialGuard.Api.Services
 {
 	public class TrustlistUserService
@@ -22,55 +20,55 @@ namespace SocialGuard.Api.Services
 
 		public IQueryable<ulong> ListUserIds() => from user in trustlistUsers.AsQueryable() select user.Id;
 
-		public async Task<TrustlistUser> FetchUserAsync(ulong id) => await (await trustlistUsers.FindAsync(u => u.Id == id)).FirstOrDefaultAsync();
+		public async Task<TrustlistUser> FetchUserAsync(ulong id) => (await trustlistUsers.FindAsync(u => u.Id == id)).FirstOrDefault();
 
 		public async Task<IEnumerable<TrustlistUser>> FetchUsersAsync(ulong[] ids) => (await trustlistUsers.FindAsync(Builders<TrustlistUser>.Filter.In(u => u.Id, ids))).ToEnumerable();
 
-		public async Task InsertNewUserAsync(TrustlistUser user, Emitter emitter)
+		public async Task InsertNewUserEntryAsync(ulong userId, TrustlistEntry entry, Emitter emitter)
 		{
-			// Check if user exists already.
-			_ = await (await trustlistUsers.FindAsync(u => u.Id == user.Id)).AnyAsync() ? throw new ArgumentOutOfRangeException(nameof(user)) : false;
-
-			await trustlistUsers.InsertOneAsync(user with
+			entry = entry with
 			{
 				EntryAt = DateTime.UtcNow,
 				LastEscalated = DateTime.UtcNow,
 				Emitter = emitter
-			});
-		}
+			};
 
-		public async Task EscalateUserAsync(TrustlistUser updated, Emitter emitter)
-		{
-			TrustlistUser current = await FetchUserAsync(updated.Id) ?? throw new ArgumentOutOfRangeException(nameof(updated));
-
-			if (current.EscalationLevel < updated.EscalationLevel)
+			if (await FetchUserAsync(userId) is null)
 			{
-				await trustlistUsers.ReplaceOneAsync(u => u.Id == current.Id, current with
+				await trustlistUsers.InsertOneAsync(new()
 				{
-					EscalationLevel = updated.EscalationLevel,
-					LastEscalated = DateTime.UtcNow,
-					EscalationNote = updated.EscalationNote,
-					Emitter = emitter
+					Id = userId,
+					Entries = new() { entry }
 				});
 			}
+			else
+			{
+				await trustlistUsers.UpdateOneAsync(
+					Builders<TrustlistUser>.Filter.Eq(u => u.Id, userId),
+					Builders<TrustlistUser>.Update.Push(u => u.Entries, entry)
+				);
+			}
 		}
 
-		public async Task ImportEntriesAsync(IEnumerable<TrustlistUser> entries, Emitter commonEmitter, DateTime importTimestamp)
+		public async Task EscalateUserAsync(ulong userId, TrustlistEntry updated, Emitter emitter)
 		{
-			foreach (TrustlistUser entry in entries)
-			{
-				if (await FetchUserAsync(entry.Id) is null)
+			TrustlistUser user = await FetchUserAsync(userId) ?? throw new ArgumentOutOfRangeException(nameof(userId), $"User {userId} not found.");
+			TrustlistEntry existing = user.Entries.First(e => e.Emitter.Login == emitter.Login);
+
+			await trustlistUsers.UpdateOneAsync(
+				Builders<TrustlistUser>.Filter.Eq(u => u.Id, userId) 
+				& Builders<TrustlistUser>.Filter.ElemMatch(u => u.Entries, Builders<TrustlistEntry>.Filter.Eq(e => e.Emitter.Login, emitter.Login)),
+				Builders<TrustlistUser>.Update.Set(u => u.Entries[-1], updated with
 				{
-					await trustlistUsers.InsertOneAsync(entry with
-					{
-						EscalationLevel = (byte)(entry.EscalationLevel is 0 ? 1 : entry.EscalationLevel),
-						EscalationNote = entry.EscalationNote ?? $"Imported from {commonEmitter.DisplayName}",
-						EntryAt = importTimestamp,
-						LastEscalated = importTimestamp,
-						Emitter = commonEmitter
-					});
-				}
-			}
+					LastEscalated = DateTime.UtcNow,
+					Emitter = emitter
+				})
+			);
+		}
+
+		public Task ImportEntriesAsync(IEnumerable<TrustlistUser> entries, Emitter commonEmitter, DateTime importTimestamp)
+		{
+			throw new NotImplementedException();
 		}
 
 		public async Task DeleteUserRecordAsync(ulong id)
