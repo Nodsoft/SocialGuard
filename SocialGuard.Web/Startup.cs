e30b63b97179742a74f61137b27e2aad4c87ef59
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2021-06-23 15:37:18 +0200

    Update PageContentLoader
    PageContentLoader is now stateless, and no longer relies on IHostedService as lifetime.

--- SocialGuard.Web/Startup.cs
+++ SocialGuard.Web/Startup.cs
@@ -26,7 +26,6 @@ namespace SocialGuard.Web
 			services.AddDistributedMemoryCache();
 
 			services.AddSingleton<PageContentLoader>();
-			services.AddHostedService<PageContentLoader>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
