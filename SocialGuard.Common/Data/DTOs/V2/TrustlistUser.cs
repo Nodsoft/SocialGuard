5ca3326f89a87a94350ed4292d04c9c515f57bc3
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2021-10-08 05:00:03 +0200

    Import Models from API to Common
    Adapted for Nullable context

---
+++ SocialGuard.Common/Data/DTOs/V2/TrustlistUser.cs
@@ -0,0 +1,27 @@
+﻿using SocialGuard.Common.Data.Models;
+
+
+
+namespace SocialGuard.Common.Data.DTOs.V2;
+
+/// <summary>
+/// Represents a Trustlist User entry.
+/// </summary>
+public record TrustlistUser
+{
+	[Required, BsonId]
+	public ulong Id { get; init; }
+
+	public DateTime EntryAt { get; set; }
+
+	public DateTime LastEscalated { get; set; }
+
+	[Required, Range(0, 3)]
+	public byte EscalationLevel { get; set; }
+
+	[Required, DisallowNull, MinLength(5), MaxLength(2000)]
+	public string EscalationNote { get; set; } = string.Empty;
+
+	[Required, DisallowNull]
+	public Emitter? Emitter { get; set; }
+}
