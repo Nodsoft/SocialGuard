45d7863c1e62873e1e1a5b0e139aa39e3469e638
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-03-27 20:24:38 +0200

    Draft DB Migrator app

--- SocialGuard.Api.DbMigrator/Program.cs
+++ SocialGuard.Api.DbMigrator/Program.cs
@@ -1,7 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+using Serilog;
+using SocialGuard.Api.Data;
 using SocialGuard.Api.DbMigrator;
+using SocialGuard.Api.DbMigrator.Models.Mongo;
 
-IHost host = Host.CreateDefaultBuilder(args)
-	.ConfigureServices(services => { services.AddHostedService<Worker>(); })
-	.Build();
+namespace SocialGuard.Api.DbMigrator;
 
-await host.RunAsync();
+public static class Program
+{
+	public static async Task Main(string[] args)
+	{
+		Log.Logger = new LoggerConfiguration()
+			.MinimumLevel.Information()
+			.Enrich.FromLogContext()
+			.WriteTo.Console()
+			.CreateLogger();
+		
+		Mappings.ConfigureMapper();
+		
+		IHost host = Host.CreateDefaultBuilder(args)
+			.ConfigureServices(ConfigureWorkerServices)
+			.UseSerilog()
+			.Build();
+
+		await host.RunAsync();
+	}
+
+	private static void ConfigureWorkerServices(HostBuilderContext hostContext, IServiceCollection services)
+	{
+		services.AddDbContext<ApiDbContext>(o =>
+			{
+				o.UseNpgsql(hostContext.Configuration.GetConnectionString("Postgres"));
+				o.UseSnakeCaseNamingConvention();
+			}
+		);
+		services.AddDbContext<AuthDbContext>(o =>
+			{
+				o.UseNpgsql(hostContext.Configuration.GetConnectionString("Postgres"));
+				o.UseSnakeCaseNamingConvention();
+			}
+		);
+		services.AddSingleton<IMongoClient>(_ => new MongoClient(hostContext.Configuration.GetConnectionString("Mongo")));
+
+		services.AddHostedService<DbMigrator>();
+	}
+}
+
