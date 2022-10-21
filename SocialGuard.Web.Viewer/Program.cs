726c49c5f1e34eb843c0a4d157c3f57452083eeb
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-10-21 14:48:50 +0200

    Update Records browser to .NET 7.0 + Small refactors

--- SocialGuard.Web.Viewer/Program.cs
+++ SocialGuard.Web.Viewer/Program.cs
@@ -4,18 +4,15 @@ using SocialGuard.Client.Http.Services;
 using SocialGuard.Web.Viewer;
 
 
-WebAssemblyHostBuilder? builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("", config =>
-{
-	config.DefaultRequestHeaders.Add("Access-Control-Allow-Origin", "*");
-});
+builder.Services.AddHttpClient("");
 
 builder.Services.AddSingleton<EmitterClient>();
 builder.Services.AddSingleton<TrustlistClient>();
 
-WebAssemblyHost? host = builder.Build();
+WebAssemblyHost host = builder.Build();
 await host.RunAsync();
